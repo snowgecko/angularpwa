2114a81d580ec6df20f213a780eb8742de3f8622
root <root@localhost>
2021-03-09 07:24:49 +0000

    first build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav'; 
+import { combineLatest } from 'rxjs';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError,ActivatedRoute, Params, Data  } from '@angular/router';
+
+//moved from https://github.com/mdbootstrap/MDB-Angular-PWA/blob/6-indexedDB-integration/src/app/app.component.ts
+//may not be relevant as this is not a schedule app - but I like the class structure that comes from 
+export interface Schedule {
+  time: string;
+  subject: string;
+  location?: string;
+  description?: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularpwa';
+
+	title = 'angularpwa';
+	networkMode = 'online';
+
+	@ViewChild('sidenav') public sideNav: MatSidenav;
+
+  	constructor(private route: ActivatedRoute, private router: Router) {   
+		navigator.onLine === true ? this.networkMode = 'online' : this.networkMode = 'offline';
+	}
+
+//this.route = location.path();
+	ngOnInit() {
+		this.router.events.subscribe(evt => { 
+		    if (evt instanceof NavigationStart) {
+				console.log( "NavigationStart id:", evt.id );
+				this.sideNav.close();
+				//console.log( "route:", evt.url );
+				//this.router.navigate(['/page']);
+				//this.route.navigate(['/page']); // navigate to other page
+				//this.routerChangeMethod(evt.url);
+		    }
+		    if (evt instanceof NavigationEnd) {
+			//console.log("NavigationEnd");
+			}
+		  });
+    }
+  	reason = '';
+	
+
+  	close(reason: string) {
+    	this.reason = reason;
+    	this.sideNav.close();
+  	}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,60 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+//import { FormBuilder, FormControl, FormGroup } from '@angular/forms' //when using notejson.
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
 
+//import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from  '@angular/material';
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
+import { NoteDetailComponent } from './note-detail/note-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+
+import { AppRoutingModule } from './app-routing.module'; // <-- NgModel lives here
+
+//temporary whilst inMemoryDataService interupts http
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+//import { InMemoryDataService } from './in-memory-data.service';
+import { NoteSearchComponent } from './note-search/note-search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { SafePipe } from './safe.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent,
+    NoteDetailComponent,
+    MessagesComponent,
+    NoteSearchComponent,
+    SafePipe
   ],
   imports: [
-    BrowserModule
+	HttpClientModule,
+	// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+	// and returns simulated server responses.
+	// Remove it when a real server is ready to receive requests.
+	//HttpClientInMemoryWebApiModule.forRoot(
+  	//	InMemoryDataService, { dataEncapsulation: false }
+	//),
+	//The forRoot() configuration method takes an InMemoryDataService class that primes the in-memory database
+    BrowserModule, 
+	FormsModule, AppRoutingModule, BrowserAnimationsModule,
+	MatExpansionModule,
+	MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    StoreModule.forRoot({}, {}),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
